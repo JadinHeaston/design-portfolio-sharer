9e107a0414d3bdc147451a5f373c40a764cecc5a
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2025-03-02 19:11:52 -0600

    Removing LDAP code.
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- includes/config.example.php
+++ includes/config.example.php
@@ -13,9 +13,3 @@ define('DB_TYPE', 'sqlsrv');
 define('DB_PORT', 1433);
 define('DB_TRUST_CERT', 1);
 define('DB_CHARSET', 'utf8mb4');
-
-//LDAP
-DEFINE('LDAP_SERVER', '');
-DEFINE('LDAP_USERNAME', '');
-DEFINE('LDAP_PASSWORD', '');
-DEFINE('LDAP_BASE_DN', '');
