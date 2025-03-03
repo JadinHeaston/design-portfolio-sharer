2244aa44443e9d9e8a78b72f437d63695841aaab
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2025-03-02 19:13:30 -0600

    Setting default database configuration to utilize sqlite.
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- includes/config.example.php
+++ includes/config.example.php
@@ -5,11 +5,11 @@ define('DISABLE_ERROR_EMAILS', false);
 define('REQUIRE_JAVASCRIPT', false); //Setting to 'true' causes a full page pop up, preventing site usage, if JS is disabled.
 
 //Database
-define('DB_HOST', 'localhost');
+define('DB_HOST', __DIR__ .  '/../data/database.sqlite3');
 define('DB_USERNAME', '');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', '');
-define('DB_TYPE', 'sqlsrv');
+define('DB_TYPE', 'sqlite');
 define('DB_PORT', 1433);
 define('DB_TRUST_CERT', 1);
 define('DB_CHARSET', 'utf8mb4');
